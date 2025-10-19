d6319e6350c79fb2305ed00464808d31b2af5bc5
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 13:46:03 +0100

    built form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,20 +5,24 @@
         @vite(['resources/themes/normal.css', 'resources/css/default.css', 'resources/css/auth.css'])
     </head>
     <body>
+        <div id="dot-container"></div>
         <div class="auth-div">
             <div class="auth-header">
                 <img class="logo" src="{{ asset('images/AllWall.png') }}" alt="">
             </div>
             <div class="auth-body">
-                <form action="login" method="post" name="login_form">
-
+                <form action="login" method="post" name="login_form" class="auth-form">
+                    @csrf
+                    <label for="email">Email<p class="required-star">*</p></label>
+                    <input type="email" name="email" id="email">
+                    <label for="password">Password<p class="required-star">*</p></label>
+                    <input type="password" name="password" id="password">
                 </form>
             </div>
             <div class="auth-footer">
 
             </div>
         </div>
-        <div id="dot-container"></div>
         @vite('resources/js/dots.js')
     </body>
 </html>
