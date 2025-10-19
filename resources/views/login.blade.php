b40552bb0fc325f85f400fa5679b0b0100ea2e1c
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 12:44:29 +0100

    ui nicer

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<html>
     <head>
         <title>Login</title>
         @vite(['resources/themes/normal.css', 'resources/css/default.css', 'resources/css/auth.css'])
@@ -8,7 +7,7 @@
     <body>
         <div class="auth-div">
             <div class="auth-header">
-                <img src="{{ asset('images/AllWall.png') }}" alt="">
+                <img class="logo" src="{{ asset('images/AllWall.png') }}" alt="">
             </div>
             <div class="auth-body">
                 <form action="login" method="post" name="login_form">
