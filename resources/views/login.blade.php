45bd26b1b4351860b799a1e31e8518e48bf488c1
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 12:10:48 +0100

    colour renames

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,9 +3,19 @@
 <html>
     <head>
         <title>Login</title>
-        @vite(['resources/css/default.css', 'resources/js/auth.js'])
+        @vite(['resources/themes/normal.css', 'resources/css/default.css', 'resources/js/auth.js'])
     </head>
     <body>
-        
+        <div class="auth-header">
+            
+        </div>
+        <div class="auth-body">
+            <form action="login" method="post" name="login_form">
+
+            </form>
+        </div>
+        <div class="auth-footer">
+
+        </div>
     </body>
 </html>
